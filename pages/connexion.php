b12c4367850c294c44eb685a9edfd8ef5c4f2c73
user <user@DESKTOP-4N4JKEG>
2020-04-22 19:47:19 +0200

    Creation de la page se connecter Ajout de la page dans le nav Mes modifications du css refusent d'être prisent en compte

---
+++ pages/connexion.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <title>Casques Nolark : Sécurité et confort, nos priorités !</title>
+        <meta charset="UTF-8">
+        <meta name="author" content="José GIL">
+        <meta name="description" content="Découvrez des casques moto dépassant même les exigences des tests de sécurité. Tous les casques Nolark au meilleur prix et avec en prime la livraison gratuite !">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../css/styles.css" rel="stylesheet" type="text/css">
+        <link href="../css/contact.css" rel="stylesheet" type="text/css">
+        <link rel="icon" href="../favicon.ico">
+    </head>
+    <body>
+        <?php
+        include('../includes/header.html.inc.php');
+        ?>
+        <h1 id="teste">Connexion :</h1>
+        <section>
+            <form>
+                <fieldset id="formu_con">
+                    <legend>J'ai déjà un compte</legend>
+                    <div><label for="i_login" ></label>Login :<input type="text" name ="i_login" id="i_login" size="35"></div>
+                    <div><label for="i_mdp" ></label>Mot de passe :<input type="password" name ="i_mdp" id="i_mdp" size="35"></div>
+                </fieldset>
+            </form>
+        </section>
+        <?php
+        include('../includes/footer.inc.php');
+        ?>
+    </body>
+</html>
+
