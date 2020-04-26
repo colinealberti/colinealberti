e539c5a2528b6fe5b4e673ebacb51d23949a92b1
user <user@DESKTOP-4N4JKEG>
2020-04-26 19:21:28 +0200

    Interface de connexion fait et opérationnel

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+<?php
+session_start();
+echo "<script>
+console.log('".$_SESSION['pseudo']."');
+</script>"
+?>
+
 <html lang="fr-FR">
     <head>
         <title>Casques Nolark : Sécurité et confort, nos priorités !</title>
@@ -15,15 +22,66 @@
         include('../includes/header.html.inc.php');
         ?>
         <h1 id="teste">Connexion :</h1>
-        <section>
-            <form>
-                <fieldset id="formu_con">
-                    <legend>J'ai déjà un compte</legend>
-                    <div><label for="i_login" ></label>Login :<input type="text" name ="i_login" id="i_login" size="35"></div>
-                    <div><label for="i_mdp" ></label>Mot de passe :<input type="password" name ="i_mdp" id="i_mdp" size="35"></div>
-                </fieldset>
-            </form>
-        </section>
+        <?php
+if (!isset($_POST['pseudo'])) //On est dans la page de formulaire
+{
+	echo '<form method="post" action="connexion.php">
+	<fieldset>
+	<legend>Connexion</legend>
+	<p>
+	<label for="pseudo">Pseudo :</label><input name="pseudo" type="text" id="pseudo" /><br />
+	<label for="password">Mot de Passe :</label><input type="password" name="password" id="password" />
+	</p>
+	</fieldset>
+	<p><input type="submit" value="Connexion" /></p></form>
+	<a href="./register.php">Pas encore inscrit ?</a>
+	 
+	</div>';
+
+}
+//On reprend la suite du code
+else
+{
+    $message='';
+    if (empty($_POST['pseudo']) || empty($_POST['password']) ) //Oublie d'un champ
+    {
+        $message = '<p>une erreur s\'est produite pendant votre identification.
+	Vous devez remplir tous les champs</p>
+	<p>Cliquez <a href="./connexion.php">ici</a> pour revenir</p>';
+    }
+    else //On check le mot de passe
+    {
+        $db = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd');
+        $query= $db->prepare('SELECT login, password, niveau from utilisateur where login= :pseudo');
+        $query->bindValue(':pseudo',$_POST['pseudo'], PDO::PARAM_STR);
+        $query->execute();
+        $data=$query->fetch();        
+	if ($data['password'] == $_POST['password']) // Acces OK !
+	{
+	    $_SESSION['pseudo'] = $data['login'];
+	    $_SESSION['level'] = $data['niveau'];
+	    $message = '<p>Bienvenue '.$data['login'].', 
+			vous êtes maintenant connecté!</p>
+			<p>Cliquez <a href="../index.php">ici</a> 
+			pour revenir à la page d accueil</p>';  
+	}
+	else // Acces pas OK !
+	{
+	    $message = '<p>Une erreur s\'est produite 
+	    pendant votre identification.<br /> Le mot de passe ou le pseudo 
+            entré n\'est pas correcte.</p><p>Cliquez <a href="./connexion.php">ici</a> 
+	    pour revenir à la page précédente
+	    <br /><br />Cliquez <a href="./index.php">ici</a> 
+	    pour revenir à la page d accueil</p>';
+	}
+    $query->CloseCursor();
+    }
+    echo $message.'</div></body></html>';
+
+}
+
+?>
+
         <?php
         include('../includes/footer.inc.php');
         ?>
